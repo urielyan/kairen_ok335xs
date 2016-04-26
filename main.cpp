26b708a52d85e33e299edd41a58b08a3525130f2
urielyan <urielyan@sina.com>
2016-04-27 07:39:07 +0800

    打印机问题

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-/*
+/*arm-linux-g++ -lts -Wl,-O1 -Wl,-rpath,/usr/local/arm/qt4.8.5/lib -o new main.o widget.o date_widget.o samplemeasurement.o countingmeasurement.o issample.o ispreheat.o spectrummeasurement.o showcountingmeasurement.o dataquery.o countdataquery.o systemsetup.o passwd.o setupdatetime.o showsamplemeasurement.o calibration.o calibrationmeasurement.o caibrateresultquery.o input_s_percentage.o query_s_count_data.o showcalibratemeasurement.o count_kb_value.o modify_kb_value.o com.o samplemeasurementquery.o hide_system.o input_person_sampleserial.o sample_data_query.o logo.o proportion.o inspectoscope.o printer.o input_machine_use_time.o painter_histogram.o spectrum_painter.o buzzer.o communication_help.o query_change_voltage.o settings_datasave.o global.o wininforlistdialog.o moc_widget.o moc_date_widget.o moc_samplemeasurement.o moc_countingmeasurement.o moc_issample.o moc_ispreheat.o moc_spectrummeasurement.o moc_showcountingmeasurement.o moc_dataquery.o moc_countdataquery.o moc_systemsetup.o moc_passwd.o moc_setupdatetime.o moc_showsamplemeasurement.o moc_calibration.o moc_calibrationmeasurement.o moc_caibrateresultquery.o moc_input_s_percentage.o moc_query_s_count_data.o moc_showcalibratemeasurement.o moc_count_kb_value.o moc_modify_kb_value.o moc_com.o moc_samplemeasurementquery.o moc_hide_system.o moc_input_person_sampleserial.o moc_sample_data_query.o moc_logo.o moc_proportion.o moc_inspectoscope.o moc_printer.o moc_input_machine_use_time.o moc_painter_histogram.o moc_spectrum_painter.o moc_buzzer.o moc_communication_help.o moc_query_change_voltage.o moc_settings_datasave.o moc_wininforlistdialog.o    -L/usr/local/arm/qt4.8.5/lib -lQtSql -L/usr/local/arm/tslib/lib -L/usr/local/arm/qt4.8.5/lib -lQtGui -lQtNetwork -lQtCore -lpthread
+yange@yange-Vostro-260:~/ok335XS/cross/kairen_ok335xs$ cp new /media/yange/E832-121D/
  *1.此程序数据保存采用QSettings类，数据保存在～/.config/shanghaikairen/下，共有两个文件 ，一个是analysis.conf和count_data.conf
  *  其中analysis.conf中保存的内容有：
  *      calibrate_input_s_n：保存用户输入的标定样硫的数据。
@@ -50,7 +51,6 @@
 
 void initSettings()
 {
-
   QCoreApplication::setOrganizationName("shanghaikairen");
   QCoreApplication::setApplicationName("analysis");
 
